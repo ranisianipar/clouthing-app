170681d610a6bd589eadc3d4d557612c77c4cf57
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-10-29 02:22:50 +0700

    done create dummy side navigation bar

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/ranilasmauli/clouthing/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ranilasmauli/clouthing/MainActivity.java
@@ -1,14 +1,40 @@
 package id.ac.ui.cs.mobileprogramming.ranilasmauli.clouthing;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
+    private ActionBarDrawerToggle mToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
