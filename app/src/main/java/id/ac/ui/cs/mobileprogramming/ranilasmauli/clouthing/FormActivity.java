04212d511f92829e6de8079b7851830193e5adb8
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-11-01 15:40:43 +0700

    add some string values, using scroll view layout, add new colors

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/ranilasmauli/clouthing/FormActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ranilasmauli/clouthing/FormActivity.java
@@ -2,13 +2,49 @@ package id.ac.ui.cs.mobileprogramming.ranilasmauli.clouthing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class FormActivity extends AppCompatActivity {
+
+    private EditText pickDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        pickDate = findViewById(R.id.et_end_date);
+        pickDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // get current date
+
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR);
+                int mMonth = c.get(Calendar.MONTH);
+                int mDay = c.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getApplicationContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                pickDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            };
+        });
     }
+
+
 }
