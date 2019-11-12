03e7599e19212efeca6a0bbc6b5b853e0bbcaf05
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-11-12 12:54:24 +0700

    succeed using time picker at form activity

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/ranilasmauli/clouthing/FormActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ranilasmauli/clouthing/FormActivity.java
@@ -72,13 +72,12 @@ public class FormActivity extends AppCompatActivity {
                 int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
                 int minute = mcurrentTime.get(Calendar.MINUTE);
                 TimePickerDialog mTimePicker;
-                mTimePicker = new TimePickerDialog(getApplicationContext(), new TimePickerDialog.OnTimeSetListener() {
+                mTimePicker = new TimePickerDialog(FormActivity.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
                         reminderTime.setText( selectedHour + ":" + selectedMinute);
                     }
                 }, hour, minute, true);//Yes 24 hour time
-                mTimePicker.setTitle("Select Time");
                 mTimePicker.show();
 
             }
